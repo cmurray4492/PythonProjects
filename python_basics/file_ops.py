5656916f02dfa10dd4516fc179c3683c79bd6829
cmurray4492 <35826709+cmurray4492@users.noreply.github.com>
2023-12-31 20:41:04 -0600

    project update 1

---
+++ python_basics/file_ops.py
@@ -0,0 +1,7 @@
+while True:
+    with open("names.txt", "a") as file:
+        name = input("Enter a name to be added: ")
+        file.write(name+'\n')
+        choice = input("Do you want to enter more names? (y/n) ")
+        if choice == 'n':
+            break
