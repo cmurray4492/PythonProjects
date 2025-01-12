75d425149cd42325f03aa480b706ac3b6dc43364
Craig Murray <cmurray4492@gmail.com>
2025-01-12 09:16:28 -0600

    Euler Setup 2

--- Project_Euler/pe1.py
+++ Project_Euler/pe1.py
@@ -2,8 +2,14 @@
 # divided equally by 3 and 5.
 
 summed = 0
-for i in range(1, 1001):
-    if i % 3 == 0 and i % 5 == 0:
+for i in range(1, 1000):
+    if i % 3 == 0 or i % 5 == 0:
         summed = summed + i
 
 print(summed)
+
+# Or a different approach
+# below uses a list which is less efficent - uses memory
+
+x = sum([n for n in range(1, 1000) if n % 3 == 0 or n % 5 == 0])
+print(x)
